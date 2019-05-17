16eff22d1a00295fc1ff994dd621428a1f027781
kaiobarb <kaiobarb@gmail.com>
2019-05-17 07:19:33 -0700

    removed video for now

--- src/App.js
+++ src/App.js
@@ -3,27 +3,84 @@ import logo from './logo.png';
 import './App.css';
 import 'bulma';
 
+import { Shaders, Node, GLSL } from "gl-react";
+import { Surface } from "gl-react-dom";
+import timeLoop from "./timeLoop";
+
+// in gl-react you need to statically define "shaders":
+const shaders = Shaders.create({
+  ColorSeparationDistortion: {
+// This is our first fragment shader in GLSL language (OpenGL Shading Language)
+// (GLSL code gets compiled and run on the GPU)
+    frag: GLSL`
+    precision highp float;
+    varying vec2 uv;
+    uniform sampler2D t;
+    uniform float time, amp, freq, moving;
+
+    vec2 lookup(vec2 offset, float amp2) {
+      return mod(
+        uv + amp2 * amp * vec2(
+          cos(freq*(uv.x+offset.x)+time),
+          sin(freq*(uv.y+offset.y)+time))
+          +vec2(moving * time/10.0, 1.0), 
+        vec2(1.0));
+    }
+    void main() {
+      float amp2 = 1.0;
+      float colorSeparation = 0.02 * mix(amp2, 1.0, 0.5);
+      vec2 orientation = vec2(1.0, 0.0);
+      gl_FragColor = vec4(vec3(
+        texture2D(t, lookup(colorSeparation * orientation, amp2)).r,
+        texture2D(t, lookup(-colorSeparation * orientation, amp2)).g,
+        texture2D(t, lookup(vec2(0.0), amp2)).b),
+        1);
+    }
+`
+// the main() function is called FOR EACH PIXELS
+// the varying uv is a vec2 where x and y respectively varying from 0.0 to 1.0.
+// we set in output the pixel color using the vec4(r,g,b,a) format.
+// see GLSL_ES_Specification_1.0.17
+  }
+});
+
+const Warp = timeLoop(({ children: t, time }) =>
+  <Node
+    shader={shaders.ColorSeparationDistortion}
+    uniforms={{
+      t,
+      time: time / 1000, // seconds is better for float precision
+      freq: 5 + 2 * Math.sin(0.0007*time),
+      amp: 0.02 + Math.max(0, 0.03*Math.cos(0.001 * time)),
+      moving: 0,
+    }}
+  />);
+
 function App() {
   return (
     <div className="App">
-      <header id="top" className="hero is-medium is-bold is-dark">
-        <div className='hero-body level'>
-          <div className='level-left column'>
-            <img src={logo} className="image" alt="logo" />
-            <div className='title'>
-              Window Pane Game
+      <header id="top" className="hero is-medium is-black">
+        <div className='hero-body'>
+          <div className='container'>
+            <div className='level'>
+              <div className='level-left column'>
+                {/* <img src={logo} className="image" alt="logo" /> */}
+                <Surface >
+                  <Warp>
+                  {logo}
+                  </Warp>
+                </Surface>
+                <div className='title'>
+                  Window Pane Game
+                </div>
+              </div>
+              <div className='level-right'>
+                Video goes here
+                {/* <video width="700" controls>
+                  <source src="trailer.mp4" type="video/mp4"/>
+                </video> */}
+              </div>
             </div>
-            <a
-              className="subtitle"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-              >
-              Learn React
-            </a>
-          </div>
-          <div className='level-right'>
-          video
           </div>
         </div>
       </header>
