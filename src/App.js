5891a0a800116c29baf240c2822167397b135cdf
kaiobarb <kaiobarb@gmail.com>
2019-05-17 04:05:52 -0700

    beggining layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'bulma';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Window Pane Game
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="top" className="hero is-medium is-bold is-dark">
+        <div className='hero-body level'>
+          <div className='level-left column'>
+            <img src={logo} className="image" alt="logo" />
+            <div className='title'>
+              Window Pane Game
+            </div>
+            <a
+              className="subtitle"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              Learn React
+            </a>
+          </div>
+          <div className='level-right'>
+          video
+          </div>
+        </div>
       </header>
+
+      <nav className='navbar is-light'>
+      <div className="navbar-brand">
+        <a className="navbar-item" href="windowpanegame.com">
+          <img src={logo} height="28" alt="logo"/>
+        </a>
+      </div>
+      <div className='navbar-menu'>
+        <a className='navbar-item' href="#top">
+          Home
+        </a> 
+
+        <a className='navbar-item' href='#about'>
+          About
+        </a>
+
+        <a className='navbar-item' href='#about'>
+          Twitter
+        </a>
+      </div>
+      </nav>
+
+      <div id='about' className='section'>
+        <div className='content'>
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ windowpanegame-build/precache-manifest.a249f0211230bc6eeaaad70044b12efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db91bac0a61a274bf056b61ab93119d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2de4c4c95705df31701",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a2de4c4c95705df31701",
+    "url": "/static/js/main.19a20057.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
